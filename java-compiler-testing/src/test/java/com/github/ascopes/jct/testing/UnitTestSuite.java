e7f2f589bf47676199523ea785bafd913b74ff58
Ashley Scopes <73482956+ascopes@users.noreply.github.com>
2022-04-11 13:15:32 +0100

    Split up unit + integration tests into suites
    These suites now run separately in Surefire. Integration tests
    will also now run in parallel now the ECJ concurrency bug is
    prevented.
    
    Also added a plugin to dump a pretty tree of the tests that
    were run into the console in Maven, because it looks kinda
    cool.

---
+++ java-compiler-testing/src/test/java/com/github/ascopes/jct/testing/UnitTestSuite.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2022 Ashley Scopes
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.ascopes.jct.testing;
+
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+import org.junit.platform.suite.api.SuiteDisplayName;
+
+/**
+ * Suite to run unit tests under.
+ *
+ * @author Ashley Scopes
+ */
+@SelectPackages("com.github.ascopes.jct.testing.unit")
+@SuiteDisplayName("Unit Tests")
+@Suite
+@SuppressWarnings("NewClassNamingConvention")
+class UnitTestSuite {
+}
