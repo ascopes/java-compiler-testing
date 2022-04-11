85b6d6f5ec8d6fe3e662dd6606570c2765d075f1
Ashley Scopes <ashleyscopes@outlook.com>
2022-04-11 12:53:06 +0100

    Prevent bug in ECJ during parallel compilation
    Appears that ECJ sometimes fails on opening a ZIP file system
    internally when we run junit in parallel. To avoid this, the
    ECJ compiler now locks runs globally so that only one instance
    of ECJ can run at a time, using a re-entrant lock.

--- java-compiler-testing/src/main/java/com/github/ascopes/jct/compilers/ecj/EcjCompiler.java
+++ java-compiler-testing/src/main/java/com/github/ascopes/jct/compilers/ecj/EcjCompiler.java
@@ -18,7 +18,9 @@ package com.github.ascopes.jct.compilers.ecj;
 
 import com.github.ascopes.jct.compilers.FlagBuilder;
 import com.github.ascopes.jct.compilers.SimpleAbstractCompiler;
+import java.util.concurrent.locks.ReentrantLock;
 import javax.tools.JavaCompiler;
+import javax.tools.JavaCompiler.CompilationTask;
 import org.apiguardian.api.API;
 import org.apiguardian.api.API.Status;
 import org.eclipse.jdt.internal.compiler.tool.EclipseCompiler;
@@ -26,11 +28,28 @@ import org.eclipse.jdt.internal.compiler.tool.EclipseCompiler;
 /**
  * Implementation of an ECJ compiler.
  *
+ * <p>ECJ is unable to run in parallel correctly, so this class will ensure that
+ * locks are acquired during the compilation process to prevent flaky tests in
+ * environments that default to concurrent test execution.
+ *
  * @author Ashley Scopes
  * @since 0.0.1
  */
 @API(since = "0.0.1", status = Status.INTERNAL)
 public final class EcjCompiler extends SimpleAbstractCompiler<EcjCompiler> {
+  // Annoyingly, ECJ seems to produce the following exception occasionally if run in
+  // parallel. To avoid this, we lock the ECJ compiler globally.
+  //
+  // java.nio.file.FileSystemAlreadyExistsException
+  //      at jdk.zipfs/...zipfs.ZipFileSystemProvider.newFileSystem(ZipFileSystemProvider.java:104)
+  //      at java.base/java.nio.file.FileSystems.newFileSystem(FileSystems.java:339)
+  //      at java.base/java.nio.file.FileSystems.newFileSystem(FileSystems.java:288)
+  //      at ecj@3.29.0/...batch.ClasspathJep247Jdk12.initialize(ClasspathJep247Jdk12.java:132)
+  //      at ecj@3.29.0/...compiler.batch.ClasspathJsr199.initialize(ClasspathJsr199.java:160)
+  //      at ecj@3.29.0/...compiler.batch.FileSystem.<init>(FileSystem.java:228)
+  //      at ecj@3.29.0/...compiler.batch.Main.getLibraryAccess(Main.java:3492)
+  //      ...
+  private static final ReentrantLock lock = new ReentrantLock();
 
   @Override
   protected String getName() {
@@ -45,5 +64,15 @@ public final class EcjCompiler extends SimpleAbstractCompiler<EcjCompiler> {
   @Override
   protected FlagBuilder createFlagBuilder() {
     return new EcjFlagBuilder();
+  }
+
+  @Override
+  protected Boolean runCompilationTask(CompilationTask task) {
+    lock.lock();
+    try {
+      return super.runCompilationTask(task);
+    } finally {
+      lock.unlock();
+    }
   }
 }
