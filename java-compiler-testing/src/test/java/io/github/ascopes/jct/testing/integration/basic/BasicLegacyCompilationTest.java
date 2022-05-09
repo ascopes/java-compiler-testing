11e3878f1c6cb02856e535e616dd7894e24ddea0
Ashley Scopes <73482956+ascopes@users.noreply.github.com>
2022-05-09 10:09:36 +0100

    Implement custom diagnostic representation

--- java-compiler-testing/src/test/java/io/github/ascopes/jct/testing/integration/basic/BasicLegacyCompilationTest.java
+++ java-compiler-testing/src/test/java/io/github/ascopes/jct/testing/integration/basic/BasicLegacyCompilationTest.java
@@ -45,7 +45,7 @@ class BasicLegacyCompilationTest {
             "com/example/HelloWorld.java",
             "package com.example;",
             "public class HelloWorld {",
-            "  public static void main(String[] args) {",
+            "  public static void main(String[] args) {?",
             "    System.out.println(\"Hello, World\");",
             "  }",
             "}"
