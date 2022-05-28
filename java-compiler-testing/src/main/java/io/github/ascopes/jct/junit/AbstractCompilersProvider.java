5e8ccc7eb29130ed1adaa6a049f6fb49badb4ca6
Ashley Scopes <73482956+ascopes@users.noreply.github.com>
2022-05-28 12:37:34 +0100

    Add missing docstring

--- java-compiler-testing/src/main/java/io/github/ascopes/jct/junit/AbstractCompilersProvider.java
+++ java-compiler-testing/src/main/java/io/github/ascopes/jct/junit/AbstractCompilersProvider.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2022 Ashley Scopes
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.github.ascopes.jct.junit;
 
 import io.github.ascopes.jct.compilers.Compilable;
@@ -10,6 +26,13 @@ import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.ArgumentsProvider;
 
+/**
+ * Internal base for defining a compiler-supplying arguments provider for Junit Jupiter
+ * parameterised test support.
+ *
+ * @author Ashley Scopes
+ * @since 0.0.1
+ */
 @API(since = "0.0.1", status = Status.INTERNAL)
 abstract class AbstractCompilersProvider implements ArgumentsProvider {
 
