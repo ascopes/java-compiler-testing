510ce255135d731ab68a804d376758afd19a47f1
Ashley Scopes <73482956+ascopes@users.noreply.github.com>
2022-09-09 09:03:51 +0100

    Update build-in-containers to only allocate TTY if one is present

--- scripts/build-in-containers.sh
+++ scripts/build-in-containers.sh
@@ -21,9 +21,9 @@
 set -o errexit
 set -o pipefail
 
-default_command="./mvnw -T8C clean package"
+default_command="./mvnw clean package"
 
-if ! command -v docker >/dev/null 2>&1; then
+if ! command -v docker; then
   echo "ERROR: docker is not installed on this system. Please ensure it is on your "
   echo "  \$PATH, and then try again."
   exit 2
@@ -39,6 +39,9 @@ function usage() {
   echo "  Version ranges can be a single digit (e.g. 17 for JDK 17), or a closed range, delimited"
   echo "  with a hyphen \`-' character (e.g. 11-17 for JDK 11, 12, 13, ..., 16, and 17)."
   echo
+  echo "Additional options can also be passed to Maven using the \$MAVEN_OPTS environment variable."
+  echo "The \$M2_HOME environment variable will be used to mount the local Maven repository. If this"
+  echo "is not defined, then ~/.m2 will be used on the host."
 }
 
 command="${default_command}"
@@ -58,8 +61,8 @@ while getopts ":c:v:h" opt; do
       usage "${0}"
       exit 1
     else
-      first_version=$(echo "${OPTARG}" | cut -d- -f 1)
-      last_version=$(echo "${OPTARG}" | cut -d- -f 2)
+      first_version="$(echo "${OPTARG}" | cut -d- -f 1)"
+      last_version="$(echo "${OPTARG}" | cut -d- -f 2)"
     fi
     ;;
   esac
@@ -72,16 +75,27 @@ if [ -z ${first_version+undef} ]; then
 fi
 
 if [ -z ${M2_HOME+undef} ]; then
-  export M2_HOME="${HOME:-$HOMEDIR}/.m2"
-fi
+  export M2_HOME="${HOME:-${HOMEDIR}}/.m2"
+  mkdir -v "${M2_HOME}" 2>/dev/null || true
+fi 
 
 workspace_dir="$(realpath $(dirname ${BASH_SOURCE[0]:-$0})/..)"
 container_workspace_dir="/src"
 m2_container_dir=/m2
 
-mkdir target 2>&1 || true
+mkdir -v target 2>/dev/null || true
 
-for version in $(seq ${first_version} ${last_version}); do
+docker_flags=()
+maven_opts=()
+
+if [ -t 1 ]; then
+  docker_flags+=("-t")
+  maven_opts+=("-Dstyle.color=always")
+else
+  maven_opts+=("-B")
+fi
+
+for version in $(seq "${first_version}" "${last_version}"); do
   # AWS Elastic Container Registry will not rate-limit us like DockerHub will, so prefer them
   # instead.
   image="public.ecr.aws/docker/library/openjdk:${version}"
@@ -95,19 +109,20 @@ for version in $(seq ${first_version} ${last_version}); do
   echo -e "\e[3;34m${image}\e[0;1;33m container...\e[0m"
 
   docker run \
-    -e "MAVEN_OPTS=-Dstyle.color=always" \
+    -e "MAVEN_OPTS=${MAVEN_OPTS} ${maven_opts[@]}" \
     -e "M2_HOME=${m2_container_dir}" \
+    --init \
     --name "jct-build-in-containers-jdk-${version}" \
-    -t \
     -u "$(id -u "${USER}")" \
     -v "$(realpath "${M2_HOME}"):${m2_container_dir}" \
     -v "${workspace_dir}:${container_workspace_dir}" \
     -w "${container_workspace_dir}" \
     --rm \
+    "${docker_flags[@]}" \
     "${image}" \
     ${command} \
     2>&1 |
-    while read line; do
+    while read -r line; do
       printf "\e[1;33m[JDK %2d]\e[0m %s\n" "${version}" "${line}"
     done
 done
