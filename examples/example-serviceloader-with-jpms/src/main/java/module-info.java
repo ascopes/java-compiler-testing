42dbcbdb172a95faf07ac2fa9ac3b955808456b0
Ashley Scopes <73482956+ascopes@users.noreply.github.com>
2022-10-08 12:21:06 +0100

    Code cleanup

--- examples/example-serviceloader-with-jpms/src/main/java/module-info.java
+++ examples/example-serviceloader-with-jpms/src/main/java/module-info.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 import io.github.ascopes.jct.examples.serviceloaderjpms.ServiceProcessor;
 import javax.annotation.processing.Processor;
 
