9cadc2225db29442f8b03a1ebf2e0e75b66a362f
Ashley <73482956+ascopes@users.noreply.github.com>
2022-10-23 12:25:59 +0100

    Update module-info.java

--- java-compiler-testing/src/main/java/module-info.java
+++ java-compiler-testing/src/main/java/module-info.java
@@ -26,7 +26,7 @@ module io.github.ascopes.jct {
   requires static transitive org.apiguardian.api;
   requires org.assertj.core;
   requires static transitive org.junit.jupiter.params;
-  requires transitive org.slf4j;
+  requires org.slf4j;
 
   exports io.github.ascopes.jct.assertions;
   exports io.github.ascopes.jct.containers;
