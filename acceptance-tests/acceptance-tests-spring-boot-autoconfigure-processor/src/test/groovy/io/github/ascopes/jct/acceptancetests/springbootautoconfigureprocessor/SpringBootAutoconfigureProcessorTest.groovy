bab63ade838b9e9702d429f23b6c0bc97f0c92d0
Ashley Scopes <73482956+ascopes@users.noreply.github.com>
2022-11-13 15:20:31 +0000

    Add tests for Spring Boot Autoconfigure Processor

---
+++ acceptance-tests/acceptance-tests-spring-boot-autoconfigure-processor/src/test/groovy/io/github/ascopes/jct/acceptancetests/springbootautoconfigureprocessor/SpringBootAutoconfigureProcessorTest.groovy
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2022 - 2022 Ashley Scopes
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.github.ascopes.jct.acceptancetests.springbootautoconfigureprocessor
+
+import io.github.ascopes.jct.compilers.JctCompiler
+import io.github.ascopes.jct.junit.JavacCompilerTest
+import io.github.ascopes.jct.pathwrappers.TempDirectory
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.parallel.Execution
+import org.junit.jupiter.api.parallel.ExecutionMode
+import org.springframework.boot.autoconfigureprocessor.AutoConfigureAnnotationProcessor
+
+import static io.github.ascopes.jct.assertions.JctAssertions.assertThatCompilation
+import static io.github.ascopes.jct.pathwrappers.TempDirectory.newTempDirectory
+
+@DisplayName("Spring Boot Autoconfigure Processor acceptance tests")
+class SpringBootAutoconfigureProcessorTest {
+
+  // TODO(ascopes): use JPMS modules when we move to Spring Framework v6.0.0
+  @DisplayName("Spring will index the application context as expected")
+  @Execution(ExecutionMode.CONCURRENT)
+  @JavacCompilerTest
+  void springWillIndexTheApplicationContextAsExpected(JctCompiler compiler) {
+    // Given
+    // Use a temporary directory here as the configuration processor uses java.io.File internally.
+    def sources = newTempDirectory("sources")
+        .createDirectory("org", "example")
+        .copyContentsFrom("src", "test", "resources", "code")
+
+    // When
+    def compilation = compiler
+        .addSourcePath(sources)
+        .addAnnotationProcessors(new AutoConfigureAnnotationProcessor())
+        .testDirectoryFactory(TempDirectory.&newTempDirectory)
+        .compile()
+
+    // Then
+    assertThatCompilation(compilation)
+        .isSuccessfulWithoutWarnings()
+        .classOutput()
+        .packages()
+        .fileExists("META-INF/spring-autoconfigure-metadata.properties")
+        .isNotEmptyFile()
+  }
+}
