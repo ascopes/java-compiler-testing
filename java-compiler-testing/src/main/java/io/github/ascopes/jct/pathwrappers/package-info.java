0e1afba757869c18354795c2d1132e5b35af00dc
Ashley <73482956+ascopes@users.noreply.github.com>
2022-11-21 07:49:00 +0000

    Add NonNull annotations to pathwrappers package

--- java-compiler-testing/src/main/java/io/github/ascopes/jct/pathwrappers/package-info.java
+++ java-compiler-testing/src/main/java/io/github/ascopes/jct/pathwrappers/package-info.java
@@ -17,7 +17,11 @@
  * Facilities for handling paths.
  */
 @API(since = "0.0.1", status = Status.EXPERIMENTAL)
+@NonNullApi
+@NonNullImpl
 package io.github.ascopes.jct.pathwrappers;
 
 import org.apiguardian.api.API;
 import org.apiguardian.api.API.Status;
+import io.github.ascopes.jct.annotations.NonNullApi;
+import io.github.ascopes.jct.annotations.NonNullImpl;
