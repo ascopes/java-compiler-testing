eca762f6af5acbac06a4b6f7ca6eb2f497d29245
Ashley <73482956+ascopes@users.noreply.github.com>
2022-11-21 07:46:33 +0000

    Add NonNull annotations to diagnostics package

--- java-compiler-testing/src/main/java/io/github/ascopes/jct/diagnostics/package-info.java
+++ java-compiler-testing/src/main/java/io/github/ascopes/jct/diagnostics/package-info.java
@@ -17,7 +17,11 @@
  * Support for collecting and representing diagnostics from compiler implementations.
  */
 @API(since = "0.0.1", status = Status.EXPERIMENTAL)
+@NonNullApi
+@NonNullImpl
 package io.github.ascopes.jct.diagnostics;
 
 import org.apiguardian.api.API;
 import org.apiguardian.api.API.Status;
+import io.github.ascopes.jct.annotations.NonNullApi;
+import io.github.ascopes.jct.annotations.NonNullImpl;
