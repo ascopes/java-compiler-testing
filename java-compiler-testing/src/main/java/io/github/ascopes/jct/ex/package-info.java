b186a319f24f054a60c7f41e923015b4078a9783
Ashley <73482956+ascopes@users.noreply.github.com>
2022-11-21 07:47:23 +0000

    Add NonNull annotations to ex package

--- java-compiler-testing/src/main/java/io/github/ascopes/jct/ex/package-info.java
+++ java-compiler-testing/src/main/java/io/github/ascopes/jct/ex/package-info.java
@@ -17,7 +17,11 @@
  * Exceptions used within this API.
  */
 @API(since = "0.0.1", status = Status.EXPERIMENTAL)
+@NonNullApi
+@NonNullImpl
 package io.github.ascopes.jct.ex;
 
 import org.apiguardian.api.API;
 import org.apiguardian.api.API.Status;
+import io.github.ascopes.jct.annotations.NonNullApi;
+import io.github.ascopes.jct.annotations.NonNullImpl;
