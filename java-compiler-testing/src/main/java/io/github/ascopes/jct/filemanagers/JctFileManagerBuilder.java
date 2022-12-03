3d0172af326964b3abcd6272dc00a6473d7f42b9
Ashley Scopes <73482956+ascopes@users.noreply.github.com>
2022-12-03 18:25:45 +0000

    Create new locations that are user-provided **before** configuring defaults

--- java-compiler-testing/src/main/java/io/github/ascopes/jct/filemanagers/JctFileManagerBuilder.java
+++ java-compiler-testing/src/main/java/io/github/ascopes/jct/filemanagers/JctFileManagerBuilder.java
@@ -382,6 +382,10 @@ public final class JctFileManagerBuilder {
   public JctFileManager createFileManager(String effectiveRelease) throws IOException {
     var fileManager = new JctFileManagerImpl(effectiveRelease);
 
+    // Copy all other explicit locations across first to give them priority.
+    locations.forEach((location, paths) ->
+        paths.forEach(path -> fileManager.addPath(location, path)));
+
     // Inherit known resources from the current JVM where appropriate.
     configureClassPath(fileManager);
     configureModulePath(fileManager);
@@ -391,17 +395,15 @@ public final class JctFileManagerBuilder {
 
     // Continue preparing the file manager with additional defaults we need.
     var fallbackFs = newFallbackFs(fileManager);
+
     // We have to manually create this one as javac will not attempt to access it lazily. Instead,
     // it will just abort if it is not present. This means we cannot take advantage of the
     // PathLocationRepository creating the roots as we try to access them for this specific case.
     createLocationIfNotPresent(fileManager, fallbackFs, StandardLocation.CLASS_OUTPUT);
+
     // Annotation processors that create files will need this directory to exist if it is to
     // work properly.
     createLocationIfNotPresent(fileManager, fallbackFs, StandardLocation.SOURCE_OUTPUT);
-
-    // Copy all other explicit locations across.
-    locations.forEach((location, paths) ->
-        paths.forEach(path -> fileManager.addPath(location, path)));
 
     return fileManager;
   }
