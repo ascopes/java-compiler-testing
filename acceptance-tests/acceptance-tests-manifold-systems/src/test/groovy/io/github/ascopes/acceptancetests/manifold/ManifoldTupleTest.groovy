d140528ab66c33435366f195ab34d191b139251c
Ashley Scopes <73482956+ascopes@users.noreply.github.com>
2022-12-04 14:53:35 +0000

    Rewire acceptance-tests-manifold-systems

--- acceptance-tests/acceptance-tests-manifold-systems/src/test/groovy/io/github/ascopes/acceptancetests/manifold/ManifoldTupleTest.groovy
+++ acceptance-tests/acceptance-tests-manifold-systems/src/test/groovy/io/github/ascopes/acceptancetests/manifold/ManifoldTupleTest.groovy
@@ -17,10 +17,11 @@ package io.github.ascopes.acceptancetests.manifold
 
 import io.github.ascopes.jct.compilers.JctCompiler
 import io.github.ascopes.jct.junit.JavacCompilerTest
+import io.github.ascopes.jct.workspaces.PathStrategy
+import io.github.ascopes.jct.workspaces.Workspace
 import org.junit.jupiter.api.DisplayName
 
 import static io.github.ascopes.jct.assertions.JctAssertions.assertThatCompilation
-import static io.github.ascopes.jct.workspaces.impl.TempDirectory.newTempDirectory
 import static org.assertj.core.api.Assertions.assertThat
 import static org.assertj.core.api.SoftAssertions.assertSoftly
 
@@ -30,47 +31,48 @@ class ManifoldTupleTest {
   @DisplayName("Tuple expressions compile as expected")
   @JavacCompilerTest(configurers = [ManifoldPluginConfigurer])
   void tupleExpressionsCompileAsExpected(JctCompiler compiler) {
-    // Given
-    def sources = newTempDirectory("sources")
-        .createDirectory("org", "example")
-        .copyContentsFrom("src", "test", "resources", "code", "tuple")
+    try (def workspace = Workspace.newWorkspace(PathStrategy.TEMP_DIRECTORIES)) {
+      // Given
+      workspace
+          .createSourcePathPackage()
+          .createDirectory("org", "example")
+          .copyContentsFrom("src", "test", "resources", "code", "tuple")
 
-    // When
-    def compilation = compiler
-        .addSourcePath(sources)
-        .compile()
+      // When
+      def compilation = compiler.compile(workspace)
 
-    // Then
-    assertThatCompilation(compilation)
-        .isSuccessfulWithoutWarnings()
+      // Then
+      assertThatCompilation(compilation)
+          .isSuccessfulWithoutWarnings()
 
-    def userType = compilation
-        .classOutputs
-        .classLoader
-        .loadClass("org.example.User")
-        .getDeclaredConstructor(long, String, int)
+      def userType = compilation
+          .classOutputs
+          .classLoader
+          .loadClass("org.example.User")
+          .getDeclaredConstructor(long, String, int)
 
-    def users = [
-        userType.newInstance(123, "Roy Rodgers McFreely", 25),
-        userType.newInstance(456, "Steve-O", 30),
-        userType.newInstance(789, "Dave Davison", 23)
-    ]
+      def users = [
+          userType.newInstance(123, "Roy Rodgers McFreely", 25),
+          userType.newInstance(456, "Steve-O", 30),
+          userType.newInstance(789, "Dave Davison", 23)
+      ]
 
-    def oldestUsers = compilation
-        .classOutputs
-        .classLoader
-        .loadClass("org.example.UserRecords")
-        .oldestUsers(users)
+      def oldestUsers = compilation
+          .classOutputs
+          .classLoader
+          .loadClass("org.example.UserRecords")
+          .oldestUsers(users)
 
-    assertThat(oldestUsers).hasSize(3)
+      assertThat(oldestUsers).hasSize(3)
 
-    assertSoftly { softly ->
-      softly.assertThat(oldestUsers[0].name).isEqualTo("Steve-O")
-      softly.assertThat(oldestUsers[0].age).isEqualTo(30)
-      softly.assertThat(oldestUsers[1].name).isEqualTo("Roy Rodgers McFreely")
-      softly.assertThat(oldestUsers[1].age).isEqualTo(25)
-      softly.assertThat(oldestUsers[2].name).isEqualTo("Dave Davison")
-      softly.assertThat(oldestUsers[2].age).isEqualTo(23)
+      assertSoftly { softly ->
+        softly.assertThat(oldestUsers[0].name).isEqualTo("Steve-O")
+        softly.assertThat(oldestUsers[0].age).isEqualTo(30)
+        softly.assertThat(oldestUsers[1].name).isEqualTo("Roy Rodgers McFreely")
+        softly.assertThat(oldestUsers[1].age).isEqualTo(25)
+        softly.assertThat(oldestUsers[2].name).isEqualTo("Dave Davison")
+        softly.assertThat(oldestUsers[2].age).isEqualTo(23)
+      }
     }
   }
 }
