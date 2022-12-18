57eb6d5bfc3521a13cda6720ab40c5047efadc8f
Ashley Scopes <73482956+ascopes@users.noreply.github.com>
2022-12-18 14:14:53 +0000

    Add error-prone acceptance test

---
+++ acceptance-tests/acceptance-tests-error-prone/src/test/groovy/io/github/ascopes/jct/acceptancetests/errorprone/ErrorProneTest.groovy
@@ -0,0 +1,83 @@
+/*
+ * Copyright (C) 2022 - 2022 Ashley Scopes
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.github.ascopes.jct.acceptancetests.errorprone
+
+import io.github.ascopes.jct.compilers.JctCompiler
+import io.github.ascopes.jct.junit.JavacCompilerTest
+import io.github.ascopes.jct.workspaces.Workspaces
+import org.junit.jupiter.api.DisplayName
+
+import static io.github.ascopes.jct.assertions.JctAssertions.assertThatCompilation
+
+@DisplayName("Error-prone acceptance tests")
+class ErrorProneTest {
+
+  @DisplayName("Happy paths work as expected")
+  @JavacCompilerTest
+  void happyPathsWorkAsExpected(JctCompiler compiler) {
+    try (def workspace = Workspaces.newWorkspace()) {
+      // Given
+      workspace
+          .createSourcePathPackage()
+          .createDirectory("org", "example")
+          .copyContentsFrom("src", "test", "resources", "code", "nullness", "happy")
+
+      // When
+      def compilation = compiler
+          .addCompilerOptions(
+              "-Xplugin:ErrorProne",
+              "-XDcompilePolicy=simple",
+          )
+          .compile(workspace)
+
+      // Then
+      assertThatCompilation(compilation)
+          .isSuccessfulWithoutWarnings()
+    }
+  }
+
+  @DisplayName("Sad paths fail as expected")
+  @JavacCompilerTest
+  void sadPathsFailAsExpected(JctCompiler compiler) {
+    try (def workspace = Workspaces.newWorkspace()) {
+      // Given
+      workspace
+          .createSourcePathPackage()
+          .createDirectory("org", "example")
+          .copyContentsFrom("src", "test", "resources", "code", "nullness", "sad")
+
+      // When
+      def compilation = compiler
+          .addCompilerOptions(
+              "-Xplugin:ErrorProne",
+              "-XDcompilePolicy=simple",
+          )
+          .compile(workspace)
+
+      // Then
+      assertThatCompilation(compilation)
+          .isFailure()
+          .diagnostics()
+          .errors()
+          .singleElement()
+          .message()
+          .startsWith(
+              "[MustBeClosedChecker] This method returns a resource which must be managed "
+                  + "carefully, not just left for garbage collection."
+          )
+    }
+  }
+}
---
+++ acceptance-tests/acceptance-tests-error-prone/src/test/resources/code/nullness/happy/HappyCase.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2022 - 2022 Ashley Scopes
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.example;
+
+import com.google.errorprone.annotations.MustBeClosed;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+/**
+ * This should pass error-prone because it always closes an auto-closeable resource.
+ */
+public class HappyCase {
+
+  /**
+   * Do something.
+   *
+   * @param args arguments.
+   * @throws IOException any exception.
+   */
+  public static void main(String[] args) throws IOException {
+    try (InputStream inputStream = inputStream()) {
+      inputStream.read();
+    }
+  }
+
+  @MustBeClosed
+  private static InputStream inputStream() throws IOException {
+    Path tempFile = Files.createTempFile("foo", "bar");
+    return Files.newInputStream(tempFile);
+  }
+}
