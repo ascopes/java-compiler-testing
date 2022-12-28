cd785fa486191558fef1673660a6a7526ada5c6e
Ashley Scopes <73482956+ascopes@users.noreply.github.com>
2022-12-28 13:20:03 +0000

    Add more sanity checks for UtilityClassTestTemplate

--- java-compiler-testing/src/test/java/io/github/ascopes/jct/tests/helpers/UtilityClassTestTemplate.java
+++ java-compiler-testing/src/test/java/io/github/ascopes/jct/tests/helpers/UtilityClassTestTemplate.java
@@ -15,18 +15,26 @@
  */
 package io.github.ascopes.jct.tests.helpers;
 
+import static java.util.function.Predicate.not;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatObject;
 import static org.assertj.core.api.InstanceOfAssertFactories.BOOLEAN;
 import static org.assertj.core.api.InstanceOfAssertFactories.array;
+import static org.junit.jupiter.api.DynamicTest.dynamicTest;
 
 import io.github.ascopes.jct.utils.UtilityClass;
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestFactory;
 import org.opentest4j.TestAbortedException;
 
 /**
@@ -101,6 +109,55 @@ public interface UtilityClassTestTemplate {
         .hasMessage("this is a utility class that cannot be initialised or extended");
   }
 
+  @DisplayName("All methods should be static")
+  @TestFactory
+  default Stream<DynamicTest> allMethodsShouldBeStatic() {
+    return getAllMethods()
+        .map(method -> {
+          var name = methodName(method);
+          return dynamicTest(name + " should be static", () -> assertThat(method)
+              .as(name)
+              .extracting(Method::getModifiers)
+              .satisfies(modifiers -> assertThat(modifiers)
+                  .as(Modifier.toString(modifiers))
+                  .extracting(Modifier::isStatic, BOOLEAN)
+                  .withFailMessage("expected method to have static modifier")
+                  .isTrue()
+              ));
+        });
+  }
+
+  @DisplayName("All fields should be static")
+  @TestFactory
+  default Stream<DynamicTest> allFieldsShouldBeStatic() {
+    return getAllFields()
+        .map(field -> {
+          var name = field.getType().getSimpleName() + " " + field.getName();
+          return dynamicTest(name + " should be static", () -> assertThat(field)
+              .as(name)
+              .extracting(Field::getModifiers)
+              .satisfies(modifiers -> assertThat(modifiers)
+                  .as(Modifier.toString(modifiers))
+                  .extracting(Modifier::isStatic, BOOLEAN)
+                  .withFailMessage("expected field to have static modifier")
+                  .isTrue()
+              ));
+        });
+  }
+
+
+  @DisplayName("All nested classes should be static")
+  @TestFactory
+  default Stream<DynamicTest> allNestedClassesShouldBeStatic() {
+    return getAllNestedClasses()
+        .map(clazz -> {
+          var name = getTypeBeingTested().getSimpleName() + "." + clazz.getSimpleName();
+          return dynamicTest(name + " should be static", () -> assertThat(clazz)
+              .as(name)
+              .isStatic());
+        });
+  }
+
   private Constructor<?> getSingleConstructor() {
     try {
       var type = getTypeBeingTested();
@@ -110,5 +167,74 @@ public interface UtilityClassTestTemplate {
     } catch (NoSuchMethodException ex) {
       throw new TestAbortedException(ex.getMessage(), ex);
     }
+  }
+
+  private Stream<Method> getAllMethods() {
+    var declared = Stream.of(getTypeBeingTested().getDeclaredMethods());
+    var inherited = Stream.of(getTypeBeingTested().getMethods());
+    var objectMethods = Stream.of(Object.class.getMethods()).collect(Collectors.toSet());
+
+    return Stream.concat(declared, inherited)
+        .distinct()
+        .filter(not(Method::isBridge))
+        .filter(not(Method::isSynthetic))
+        .filter(not(objectMethods::contains));
+  }
+
+  private Stream<Field> getAllFields() {
+    var declared = Stream.of(getTypeBeingTested().getDeclaredFields());
+    var inherited = Stream.of(getTypeBeingTested().getFields());
+    var objectFields = Stream.of(Object.class.getFields()).collect(Collectors.toSet());
+
+    return Stream.concat(declared, inherited)
+        .distinct()
+        .filter(not(Field::isSynthetic))
+        .filter(not(Field::isEnumConstant))
+        .filter(not(objectFields::contains));
+  }
+
+  private Stream<Class<?>> getAllNestedClasses() {
+    var declared = Stream.of(getTypeBeingTested().getDeclaredClasses());
+    var inherited = Stream.of(getTypeBeingTested().getClasses());
+    var objectClasses = Stream.of(Object.class.getClasses()).collect(Collectors.toSet());
+
+    return Stream.concat(declared, inherited)
+        .distinct()
+        .filter(not(Class::isSynthetic))
+        .filter(not(objectClasses::contains));
+  }
+
+  private String methodName(Method method) {
+    var sb = new StringBuilder(method.getReturnType().getSimpleName())
+        .append(" ")
+        .append(method.getName())
+        .append("(");
+
+    var firstParam = true;
+
+    for (var param : method.getParameters()) {
+      if (param.isImplicit()) {
+        continue;
+      }
+
+      if (firstParam) {
+        firstParam = false;
+      } else {
+        sb.append(", ");
+      }
+
+      sb.append(param.getType().getSimpleName());
+
+      if (param.isVarArgs()) {
+        sb.append("...");
+      }
+
+      if (param.isNamePresent()) {
+        sb.append(" ").append(param.getName());
+      }
+    }
+
+    return sb.append(")")
+        .toString();
   }
 }
