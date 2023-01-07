fc08c681cb1fead9194ad4259b090376dddb3196
Badrri Narayanan S <badrri1995@gmail.com>
2023-01-07 23:00:12 +0530

    updates JctFileManagerImplTest with 2 unit tests.

--- java-compiler-testing/src/test/java/io/github/ascopes/jct/tests/unit/filemanagers/JctFileManagerImplTest.java
+++ java-compiler-testing/src/test/java/io/github/ascopes/jct/tests/unit/filemanagers/JctFileManagerImplTest.java
@@ -1,2 +1,29 @@
-package io.github.ascopes.jct.tests.unit.filemanagers;public class JctFileManagerImplTest {
+package io.github.ascopes.jct.tests.unit.filemanagers;
+
+import io.github.ascopes.jct.filemanagers.JctFileManager;
+import io.github.ascopes.jct.filemanagers.ModuleLocation;
+import io.github.ascopes.jct.filemanagers.impl.JctFileManagerImpl;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@DisplayName("JctFileManager Tests")
+public class JctFileManagerImplTest {
+
+  @Test
+  @DisplayName("Tests whether we get instance of JctFileManager")
+  void testGettingJctFileManagerImplInstance() {
+    assertTrue(JctFileManagerImpl.forRelease("test") instanceof JctFileManagerImpl);
+  }
+
+  @Test
+  @DisplayName("Tests whether we get instance of JctFileManager")
+  void testIfNullPointerExceptionThrownIfReleaseNull() {
+    assertThrows(NullPointerException.class,() -> {
+      JctFileManagerImpl.forRelease(null);
+    });
+  }
+
 }
