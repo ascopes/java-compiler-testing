c0fd76028ffb2f314ef0fc28ae3951a6c62a9659
Badrri Narayanan S <badrri1995@gmail.com>
2023-01-08 21:45:34 +0530

    adds 2 more testcases. Refactors testcases to use AssertJ instead of Junit and modifies @DisplayName to be consistent with project style.

--- java-compiler-testing/src/test/java/io/github/ascopes/jct/tests/unit/filemanagers/JctFileManagerImplTest.java
+++ java-compiler-testing/src/test/java/io/github/ascopes/jct/tests/unit/filemanagers/JctFileManagerImplTest.java
@@ -3,27 +3,67 @@ package io.github.ascopes.jct.tests.unit.filemanagers;
 import io.github.ascopes.jct.filemanagers.JctFileManager;
 import io.github.ascopes.jct.filemanagers.ModuleLocation;
 import io.github.ascopes.jct.filemanagers.impl.JctFileManagerImpl;
+import io.github.ascopes.jct.workspaces.PathRoot;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import java.nio.file.Path;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import javax.tools.JavaFileManager.Location;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DisplayName("JctFileManager Tests")
-public class JctFileManagerImplTest {
+class JctFileManagerImplTest {
 
   @Test
-  @DisplayName("Tests whether we get instance of JctFileManager")
+  @DisplayName("generates JctFileManager instance for a release")
   void testGettingJctFileManagerImplInstance() {
-    assertTrue(JctFileManagerImpl.forRelease("test") instanceof JctFileManagerImpl);
+    assertThat(JctFileManagerImpl.forRelease("test")).isInstanceOf(JctFileManagerImpl.class);
   }
 
   @Test
-  @DisplayName("Tests whether we get instance of JctFileManager")
+  @DisplayName("null release is disallowed")
   void testIfNullPointerExceptionThrownIfReleaseNull() {
-    assertThrows(NullPointerException.class,() -> {
+    assertThatThrownBy(() -> {
       JctFileManagerImpl.forRelease(null);
-    });
+    }).isInstanceOf(NullPointerException.class)
+        .hasMessage("release");
+  }
+
+  @Test
+  @DisplayName("adds package location to JctFileManager")
+  void testAddPathForPackageLocation() {
+    var packageLocation = mock(Location.class);
+    var pathRoot = mock(PathRoot.class);
+    var path = mock(Path.class);
+
+    // we mock path because it is needed by AbstractPackageContainerGroup
+    given(pathRoot.getPath()).willReturn(path);
+
+    var jctFileManager = JctFileManagerImpl.forRelease("test");
+    jctFileManager.addPath(packageLocation, pathRoot);
+    assertThat(jctFileManager.hasLocation(packageLocation)).isEqualTo(true);
+  }
+
+  @Test
+  @DisplayName("adds output location to JctFileManager")
+  void testAddPathForOutputLocation() {
+    var outputLocation = mock(Location.class);
+    var pathRoot = mock(PathRoot.class);
+    var path = mock(Path.class);
+
+    // we mock path because it is needed by AbstractPackageContainerGroup
+    given(pathRoot.getPath()).willReturn(path);
+    given(outputLocation.isOutputLocation()).willReturn(true);
+
+    var jctFileManager = JctFileManagerImpl.forRelease("test");
+    jctFileManager.addPath(outputLocation, pathRoot);
+    assertThat(jctFileManager.hasLocation(outputLocation)).isEqualTo(true);
   }
 
 }
--- java-compiler-testing/src/test/java/io/github/ascopes/jct/tests/unit/filemanagers/package-info.java
+++ java-compiler-testing/src/test/java/io/github/ascopes/jct/tests/unit/filemanagers/package-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2022 - 2023 Badrri Narayanan S
+ * Copyright (C) 2022 - 2023, the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
