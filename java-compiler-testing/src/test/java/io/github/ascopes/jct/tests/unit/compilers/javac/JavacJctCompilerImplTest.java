50328988402287c368cc63c6df2f5848901588ab
Ashley <73482956+ascopes@users.noreply.github.com>
2023-01-20 09:27:15 +0000

    Do not enable Java 8 if using javac on Java 20 or newer
    Java 20 reports deprecation/obsolescence warnings for Java 8.
    
    Signed-off-by: Ashley <73482956+ascopes@users.noreply.github.com>

--- java-compiler-testing/src/test/java/io/github/ascopes/jct/tests/unit/compilers/javac/JavacJctCompilerImplTest.java
+++ java-compiler-testing/src/test/java/io/github/ascopes/jct/tests/unit/compilers/javac/JavacJctCompilerImplTest.java
@@ -24,7 +24,6 @@ import static org.mockito.Mockito.when;
 
 import io.github.ascopes.jct.compilers.javac.JavacJctCompilerImpl;
 import io.github.ascopes.jct.compilers.javac.JavacJctFlagBuilderImpl;
-import java.util.Random;
 import javax.lang.model.SourceVersion;
 import javax.tools.JavaCompiler;
 import javax.tools.ToolProvider;
@@ -33,7 +32,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 /**
  * {@link JavacJctCompilerImpl} tests.
@@ -133,36 +131,107 @@ class JavacJctCompilerImplTest {
   }
 
   @DisplayName("the earliest supported version int has the expected value")
-  @CsvSource({
-      "true, 9",
-      "false, 8",
+  @CsvSource(useHeadersInDisplayName = true, value = {
+      "SourceVersion.latest(), modules, expectedResult",
+      "                     9,   false,              8",
+      "                     9,    true,              9",
+      "                    10,   false,              8",
+      "                    10,    true,              9",
+      "                    11,   false,              8",
+      "                    11,    true,              9",
+      "                    12,   false,              8",
+      "                    12,    true,              9",
+      "                    13,   false,              8",
+      "                    13,    true,              9",
+      "                    14,   false,              8",
+      "                    14,    true,              9",
+      "                    15,   false,              8",
+      "                    15,    true,              9",
+      "                    16,   false,              8",
+      "                    16,    true,              9",
+      "                    17,   false,              8",
+      "                    17,    true,              9",
+      "                    18,   false,              8",
+      "                    18,    true,              9",
+      "                    19,   false,              8",
+      "                    19,    true,              9",
+      // JDK-20 marks Java 8 as obsolete, so we do not support it.
+      // We default to Java 9 regardless in this case.
+      "                    20,   false,              9",
+      "                    20,    true,              9",
+      // JDK-21 still considers Java 8 to be the minimum version.
+      "                    21,   false,              9",
+      "                    21,    true,              9",
   })
-  @ParameterizedTest(name = "expect {1} when modules = {0}")
-  void theEarliestSupportedVersionIntHasTheExpectedValue(boolean modules, int expect) {
-    // Then
-    assertThat(JavacJctCompilerImpl.getEarliestSupportedVersionInt(modules))
-        .isEqualTo(expect);
-  }
-
-  @DisplayName("the latest supported version int has the expected value")
-  @ValueSource(booleans = {true, false})
-  @ParameterizedTest(name = "for modules = {0}")
-  @SuppressWarnings("ResultOfMethodCallIgnored")
-  void theLatestSupportedVersionIntHasTheExpectedValue(boolean modules) {
+  @ParameterizedTest(name = "expect {2} when {0} and {1}")
+  void earliestSupportedVersionReturnsTheExpectedValue(
+      int latest, boolean modules, int expectedResult
+  ) {
     // Given
     try (var sourceVersionMock = mockStatic(SourceVersion.class)) {
-      var latestSupportedOrdinal = 11 + new Random().nextInt(10);
-
-      var latestSupportedEnum = mock(SourceVersion.class);
-      when(latestSupportedEnum.ordinal())
-          .thenReturn(latestSupportedOrdinal);
-
       sourceVersionMock.when(SourceVersion::latestSupported)
-          .thenReturn(latestSupportedEnum);
+          .then(ctx -> {
+            var sourceVersion = mock(SourceVersion.class);
+            when(sourceVersion.ordinal()).thenReturn(latest);
+            return sourceVersion;
+          });
+
+      // When
+      var actualResult = JavacJctCompilerImpl.getEarliestSupportedVersionInt(modules);
 
       // Then
-      assertThat(JavacJctCompilerImpl.getLatestSupportedVersionInt(modules))
-          .isEqualTo(latestSupportedOrdinal);
+      assertThat(actualResult).isEqualTo(expectedResult);
+    }
+  }
+
+  @DisplayName("the earliest supported version int has the expected value")
+  @CsvSource(useHeadersInDisplayName = true, value = {
+      "SourceVersion.latest(), modules, expectedResult",
+      "                     9,   false,              9",
+      "                     9,    true,              9",
+      "                    10,   false,             10",
+      "                    10,    true,             10",
+      "                    11,   false,             11",
+      "                    11,    true,             11",
+      "                    12,   false,             12",
+      "                    12,    true,             12",
+      "                    13,   false,             13",
+      "                    13,    true,             13",
+      "                    14,   false,             14",
+      "                    14,    true,             14",
+      "                    15,   false,             15",
+      "                    15,    true,             15",
+      "                    16,   false,             16",
+      "                    16,    true,             16",
+      "                    17,   false,             17",
+      "                    17,    true,             17",
+      "                    18,   false,             18",
+      "                    18,    true,             18",
+      "                    19,   false,             19",
+      "                    19,    true,             19",
+      "                    20,   false,             20",
+      "                    20,    true,             20",
+      "                    21,   false,             21",
+      "                    21,    true,             21",
+  })
+  @ParameterizedTest(name = "expect {2} when {0} and {1}")
+  void latestSupportedVersionReturnsTheExpectedValue(
+      int latest, boolean modules, int expectedResult
+  ) {
+    // Given
+    try (var sourceVersionMock = mockStatic(SourceVersion.class)) {
+      sourceVersionMock.when(SourceVersion::latestSupported)
+          .then(ctx -> {
+            var sourceVersion = mock(SourceVersion.class);
+            when(sourceVersion.ordinal()).thenReturn(latest);
+            return sourceVersion;
+          });
+
+      // When
+      var actualResult = JavacJctCompilerImpl.getLatestSupportedVersionInt(modules);
+
+      // Then
+      assertThat(actualResult).isEqualTo(expectedResult);
     }
   }
 }
