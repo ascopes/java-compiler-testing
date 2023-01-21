0d15d7ddf50d453995ae55e9225cf791f6a874d7
Ashley <73482956+ascopes@users.noreply.github.com>
2023-01-21 11:54:21 +0000

    Pass --huge to xsltproc to handle huge JUnit CDATA blocks
    Signed-off-by: Ashley <73482956+ascopes@users.noreply.github.com>

--- scripts/prepare-test-outputs-for-merge.sh
+++ scripts/prepare-test-outputs-for-merge.sh
@@ -115,7 +115,8 @@ for report in $(find-all-surefire-reports) $(find-all-failsafe-reports); do
   report_count="$((report_count+1))"
   new_report="${report/.xml/-java-${ci_java_version}-${ci_os}.xml}"
   prefix="[Java-${ci_java_version}-${ci_os}]"
-  run <<< "xsltproc --stringparam prefix '${prefix}' '${surefire_prefix_xslt}' '${report}' > '${new_report}'"
+  # Pass --huge to work around some issues with JUnit reports having massive CDATA blocks.
+  run <<< "xsltproc --huge --stringparam prefix '${prefix}' '${surefire_prefix_xslt}' '${report}' > '${new_report}'"
   run <<< "rm '${report}'"
 done
 success "Updated ${report_count} test reports"
