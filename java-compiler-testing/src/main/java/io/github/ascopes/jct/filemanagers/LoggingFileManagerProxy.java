67beccad38605e1982519fc369344fbdbc5e2074
Ashley <73482956+ascopes@users.noreply.github.com>
2023-01-22 10:03:52 +0000

    Make logging file manager proxy public
    Add LoggingFileManagerProxy to the stable API
    
    Signed-off-by: Ashley <73482956+ascopes@users.noreply.github.com>

--- java-compiler-testing/src/main/java/io/github/ascopes/jct/filemanagers/LoggingFileManagerProxy.java
+++ java-compiler-testing/src/main/java/io/github/ascopes/jct/filemanagers/LoggingFileManagerProxy.java
@@ -45,7 +45,7 @@ import org.slf4j.LoggerFactory;
  * @author Ashley Scopes
  * @since 0.0.1
  */
-@API(since = "0.0.1", status = Status.INTERNAL)
+@API(since = "0.0.1", status = Status.STABLE)
 @ThreadSafe
 public final class LoggingFileManagerProxy implements InvocationHandler {
 
