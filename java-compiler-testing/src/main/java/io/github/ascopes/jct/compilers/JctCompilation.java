0bf986f8b1ed076ad9bcd17a7b6e2616d2806470
Ashley Scopes <73482956+ascopes@users.noreply.github.com>
2023-03-26 12:16:07 +0100

    Remove default override for JctCompilation.getArguments for v1.0.0

--- java-compiler-testing/src/main/java/io/github/ascopes/jct/compilers/JctCompilation.java
+++ java-compiler-testing/src/main/java/io/github/ascopes/jct/compilers/JctCompilation.java
@@ -39,19 +39,10 @@ public interface JctCompilation {
   /**
    * Get the command line arguments that were passed to the compiler.
    *
-   * <p>Note that this throws an unsupported operation exception by default
-   * to prevent breaking existing functionality. In v1.0.0, this will become
-   * required behaviour.
-   *
    * @return the command line arguments.
-   * @since 0.5.0
    */
   @API(since = "0.5.0", status = Status.STABLE)
-  default List<String> getArguments() {
-    throw new UnsupportedOperationException(
-        "This operation is not implemented, but will be mandatory in v1.0.0"
-    );
-  }
+  List<String> getArguments();
 
   /**
    * Determine if warnings were treated as errors.
