77770f21b895f8f692bfe93deb725509ee6cd375
Ashley Scopes <73482956+ascopes@users.noreply.github.com>
2023-05-07 14:21:21 +0100

    Sort imports

--- java-compiler-testing/src/main/java/module-info.java
+++ java-compiler-testing/src/main/java/module-info.java
@@ -103,9 +103,9 @@ module io.github.ascopes.jct {
   /// DEPENDENCIES ///
   ////////////////////
 
+  requires com.github.marschall.memoryfilesystem;
   requires java.compiler;
   requires java.management;
-  requires com.github.marschall.memoryfilesystem;
   requires me.xdrop.fuzzywuzzy;
   requires static transitive org.apiguardian.api;
   requires org.assertj.core;
