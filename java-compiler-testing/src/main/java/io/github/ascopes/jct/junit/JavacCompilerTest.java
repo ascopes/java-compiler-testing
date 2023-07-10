11526e79de6b0a8d34496209ea1d7db1db489ae5
ascopes <73482956+ascopes@users.noreply.github.com>
2023-07-10 08:31:43 +0100

    Update JavacCompilerTest.java to not allow annotation on classes
    Usage of the annotation on classes is undocumented and not actively supported.
    
    Signed-off-by: ascopes <73482956+ascopes@users.noreply.github.com>

--- java-compiler-testing/src/main/java/io/github/ascopes/jct/junit/JavacCompilerTest.java
+++ java-compiler-testing/src/main/java/io/github/ascopes/jct/junit/JavacCompilerTest.java
@@ -80,12 +80,15 @@ import org.junit.jupiter.params.provider.ArgumentsSource;
 @Documented
 @ParameterizedTest(name = "for compiler \"{0}\"")
 @Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.ANNOTATION_TYPE, ElementType.METHOD, ElementType.TYPE})
-@TestTemplate
 @Tags({
     @Tag("java-compiler-testing-test"),
     @Tag("javac-test")
 })
+@Target({
+    ElementType.ANNOTATION_TYPE, 
+    ElementType.METHOD,
+})
+@TestTemplate
 public @interface JavacCompilerTest {
 
   /**
