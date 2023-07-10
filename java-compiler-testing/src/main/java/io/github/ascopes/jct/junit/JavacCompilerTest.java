68676411b307b8bb645c95ce29209b9d13d2a333
ascopes <73482956+ascopes@users.noreply.github.com>
2023-07-10 08:28:44 +0100

    Remove unneeded imports from JavacCompilerTest.java
    Signed-off-by: ascopes <73482956+ascopes@users.noreply.github.com>

--- java-compiler-testing/src/main/java/io/github/ascopes/jct/junit/JavacCompilerTest.java
+++ java-compiler-testing/src/main/java/io/github/ascopes/jct/junit/JavacCompilerTest.java
@@ -18,7 +18,6 @@ package io.github.ascopes.jct.junit;
 import io.github.ascopes.jct.compilers.JctCompilerConfigurer;
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
-import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
